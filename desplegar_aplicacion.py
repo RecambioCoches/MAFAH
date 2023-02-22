89a4c5f852d5e7465924aae1ed1277c37ec3dde3
Gabriel Cutas <bcutas12@gmail.com>
2023-02-22 10:12:56 +0100

    automatizacion despliegue local

--- desplegar_aplicacion.py
+++ desplegar_aplicacion.py
@@ -1,12 +1,17 @@
-import platform
-import subprocess
 import os
+#ENTRA EN LA CARPETA SERVER
 os.chdir('server')
+#INSTALA LAS DEPENDECIAS DEL SYMFONY
 os.system("composer i")
+#INICIA EL SERVIDOR DE SYMFONY 
 os.system("start symfony server:start ")
+#VUELVE AL DIRECTORIO PRINCIPAL
 os.chdir('..')
+#ENTRA A LA CARPETA CLIENT
 os.chdir('client')
+#INSTALA LAS DEPENDECIAS DEL ANGULAR
 os.system("npm i")
+#INICIA EL SERVIDOR DE ANGULAR
 os.system("start ng serve -o ")
 os.chdir('..')
 os.system('cls')
